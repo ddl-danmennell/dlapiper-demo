83063f3ddf2aa274fb289473e860e90e0e67a31f
Daniel Mennell <dan.mennell@dominodatalab.com>
2024-08-14 09:05:21 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/dmennell/RAG_Demo_App?executionId=66bcc1d1d747671610f99d82

--- API_streamlit_app.py
+++ API_streamlit_app.py
@@ -61,10 +61,10 @@ with container:
         answer = None
         with st.spinner("Searching for the answer..."):
             # You will need to update the "post" and "auth" details for the model you have deployed
-            response = requests.post("https://se-demo.domino.tech:443/models/65b2846db2e5737d566de52e/latest/model",
+            response = requests.post("https://se-demo.domino.tech:443/models/66bcc5cb2c858e64f6985a87/latest/model",
                 auth=(
-                    "dWyCVvxpastxkWhGf0TIXMXpsWWGnSrfGzFAV7yr3O33f4Hs3qmeQB5sWxbfrLy7",
-                    "dWyCVvxpastxkWhGf0TIXMXpsWWGnSrfGzFAV7yr3O33f4Hs3qmeQB5sWxbfrLy7"
+                    "wRga1iBeK8CNJwg1ZcnTsp4BDDVFtKAq9IAkM2E3qHcUyF6pTzPwpbnNkGGpZyIy",
+                    "wRga1iBeK8CNJwg1ZcnTsp4BDDVFtKAq9IAkM2E3qHcUyF6pTzPwpbnNkGGpZyIy"
                 ),
                 json={
                     # This is the data payload for the API
