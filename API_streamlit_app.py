2b0de32b24d0bbd97c4fd3e327a5629482e5a48e
Daniel Mennell <dan.mennell@dominodatalab.com>
2024-08-14 12:51:13 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/dmennell/RAG_Demo_App?executionId=66bcc1d1d747671610f99d82

--- API_streamlit_app.py
+++ API_streamlit_app.py
@@ -38,7 +38,7 @@ if clear_button:
     ]
 
 # Header Image - You can change this to suit your application
-st.image("images/dla.png")
+st.image("images/dla2.png")
 # container for chat history
 response_container = st.container()
 # container for text box
